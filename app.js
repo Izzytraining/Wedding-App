d0a03df08cac6690fd6eb962cc35878dbb22db69
Izzytraining <izzyjhalliday23@gmail.com>
2023-01-12 17:19:13 +0000

    changes

--- app.js
+++ app.js
@@ -72,15 +72,10 @@ addreception.addEventListener("click", function () {
   });
 });
 
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  // true for mobile device
+// true for mobile device
 //  document.write("mobile device");
 //} else {
-  // false for not mobile device
- // document.write("not mobile device");
+// false for not mobile device
+// document.write("not mobile device");
 //}
 //console.log;
